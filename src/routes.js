4db29b4bc4d77f4ac63f59b2b05bdc7fad69040a
Jhordan S <jhordan.kendy@outlook.com>
2024-01-22 17:51:33 -0300

    add route of transfer

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import UserController from './controllers/UserController'
 import DepositController from './controllers/DepositController'
-import Transferencia from './controllers/Transferencia'
+import TansferController from './controllers/TransferController'
 
 const routes = new Router()
 
 routes.post('/create-user', UserController.store)
 routes.post('/account', DepositController.index)
 routes.post('/deposit', DepositController.store)
-routes.post('/transferencia', Transferencia.update)
+routes.post('/transfer', TansferController.update)
 
 export default routes
