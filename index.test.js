28f996fed4f036f7f65a9155318794f3420d79ce
Jhordan S <jhordan.kendy@outlook.com>
2024-01-31 18:15:49 -0300

    add teste de falha

--- index.test.js
+++ index.test.js
@@ -1,21 +1,55 @@
-import assert from 'assert'
-import User from './src/database/models/User.js'
-import UserService from './src/services/UserService.js'
-
+import assert from "assert";
+import { User } from "./src/entities/User.js";
+import Service from "./service.js";
 
 // should throw an error when cpf is less than 11 characters long
 {
-    const params = {
-    name : "Alex",
-    lastname : "Poatan",
-    cpf : "55555555555",
-    cnpj : "55555555555",
-    email : "alex@email.com",
-    password : "123456",
-    businessman: false
-    }
+  const params = {
+    name: "Ax",
+    lastname: "Poatan",
+    cpf: "55555555555",
+    cnpj: "55555555555",
+    email: "alex@email.com",
+    password: "123456",
+    businessman: false,
+  };
 
-    const user = new User()
+  const user = new User({
+    onCreate: () => {},
+    service: new Service(),
+  });
 
-    user.create(params)
+  assert.rejects(
+    () => user.create(params),
+    { message: "user must be higher than 3 " },
+    "it should throw an error with wrong user name"
+  );
+
+
 }
+
+// should save user successfully
+{
+    const params = {
+      name: "Alex",
+      lastname: "Poatan",
+      cpf: "55555555555",
+      cnpj: "55555555555",
+      email: "alex@email.com",
+      password: "123456",
+      businessman: false,
+    };
+  
+    const user = new User({
+      onCreate: () => {},
+      service: new Service(),
+    });
+  
+    assert.rejects(
+      () => user.create(params),
+      { message: "user must be higher than 3 " },
+      "it should throw an error with wrong user name"
+    );
+  
+    
+  }
